c2eff2cf1e4cb26e47baa01fb31d50d08c22eac5
yoelio <yoswed123@gmail.com>
2019-12-19 16:20:44 -0500

    added replace previous closer functionality

--- content.js
+++ content.js
@@ -12,8 +12,8 @@ var professional = [
     "Respectfully,",
     "All the best,",
     "Best regards",
-    "Best wishes,",
-]
+    "Best wishes,"
+];
 
 var impersonal = [
     "Fond regards,",
@@ -23,27 +23,33 @@ var impersonal = [
     "With appreciation,",
     "With gratitude,",
     "Yours sincerely,"
-]
+];
 
-chrome.runtime.onMessage.addListener((message, sender) => {
+var previousCloser;
+
+chrome.runtime.onMessage.addListener((message, _) => {
     if (document.readyState === "complete") {
         var body = document.querySelector('div[aria-label="Message Body"]');
-        var currentBody = body.innerHTML;
         var closer;
         console.log(message);
         switch(message.type) {
             case "professional": 
-                closer = "<br/><br/>" + professional[parseInt(Math.random() * professional.length)];
+                closer = "<br><br>" + professional[parseInt(Math.random() * professional.length)];
                 break;
             case "impersonal":
-                closer = "<br/><br/>" + impersonal[parseInt(Math.random() * impersonal.length)];
+                closer = "<br><br>" + impersonal[parseInt(Math.random() * impersonal.length)];
                 break;
         }
-    
-        if (currentBody) {
-            body.innerHTML = currentBody + closer;
+
+        if (body.innerHTML) {
+            if (previousCloser) {
+                body.innerHTML = body.innerHTML.replace(previousCloser, closer);
+            } else {
+                body.innerHTML += closer;
+            }
         } else {
-            body.innerHTML = closer
+            body.innerHTML = closer;
         }
+        previousCloser = closer;
     }
 });
